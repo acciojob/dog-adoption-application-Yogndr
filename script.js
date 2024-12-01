42e7a7a42847bb254a0cc7fe1a4e937de7e2f558
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-01 11:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here
-https://www.svgrepo.com/show/345221/three-dots.svg
+//your code heresplitter
+CSS
+
+1
+2
+
+/* Your CSS Code here. */
+
+Javascript
+
+1
+2
+
+https://www.svgrepo.com/show/345221/three-dots.svghttps://www.svgrepo.com/show/345221/three-dots.svg
